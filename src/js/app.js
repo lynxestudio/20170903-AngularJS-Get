35db21357d0b67ac6c496fa83863aac62b4696c0
Martin Ortega <lynxestudio@outlook.com>
2023-11-07 16:19:13 -0600

    Add source code to the project.

---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+﻿
+(function () {
+    var app = angular.module('getExample', []);
+    app.controller('booksCtrl', function ($scope, $http)
+    {
+        var url = "/books.json";
+        $scope.showBooks = function () {
+            $http.get(url, { responseType: "json" }).success(function (response) {
+                $scope.books = response;
+            }).error(function (data, status, headers, config) {
+                alert("There was a error.");
+            });
+        }
+    });
+})();
